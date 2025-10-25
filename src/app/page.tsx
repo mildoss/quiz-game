9549c6856700d782495422261db0e2726a887d6b
mildoss <vlad.maslo.05@gmail.com>
2025-10-25 14:37:57 +0300

    feat: display username in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import Link from "next/link";
+"use client"
+
+import {useSelector} from "react-redux";
+import {selectToken, selectUsername} from "@/store/authSlice";
 
 export default function Home() {
+  const token = useSelector(selectToken);
+  const username = useSelector(selectUsername);
+
   return (
     <div className="h-full flex flex-col justify-center items-center">
-      <h1 className="text-7xl text-center">Quiz App</h1>
-      <Link href="/register">Register</Link>
-      <Link href="/login">Login</Link>
+      <h1 className="text-5xl text-center">Quiz App</h1>
+      {token && <p>Your successfully authorized!</p>}
+      {token && <p>Your username: {username}</p>}
     </div>
   );
 }
